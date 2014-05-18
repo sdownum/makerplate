709e8ca80bf03131dc27374e90d9f17cad382022
Steven Downum <devilaether@gmail.com>
2014-05-18 17:30:50 -0500

    LCD character display support
    *Added support for character displays with the Adafruit i2c/SPI LCD
    Backpack.
    * Code currently uses i2c bus

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 This is a project for a Arduino (or Atmel AVR) based programmable electronic nameplate to proudly hang in your office or cubicle.
 
+Currently supports character displays using the Adafruit i2c/SPI LCD Backpack, over i2c.
+
+Requires the Adafruit Liquid Crystal library, found here:
+https://github.com/adafruit/LiquidCrystal
--- makerplate.ino
+++ makerplate.ino
@@ -1,10 +1,15 @@
 
+#include "Wire.h"
+#include "LiquidCrystal.h"
+
+LiquidCrystal lcd(0);
 int output_led = 13;
 char buf[256];
 
 void setup()
 {
    pinMode(output_led, OUTPUT);
+   lcd.begin(16, 2);
    Serial.begin(9600);
    Serial.println("makerplate debug");
    Serial.println("****************");
@@ -22,6 +27,8 @@ void loop()
       Serial.print("New display message is: ");
       Serial.println(buf);
       Serial.println("\n");
+      lcd.setCursor(0, 1);
+      lcd.print(buf);
       delay(1000);
       digitalWrite(output_led, LOW);
       Serial.println("Type a new display message:");
